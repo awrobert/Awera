5ee7b6335d1816b6840387c7efbd4a0c7fd686d2
Robert AW <rmouze@mail.ru>
2017-03-16 15:18:49 +0300

    ss

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
-<h2>ТЕСТ</h2>
+<h2>ТЕssСТ</h2>
 <div id="slider"></div>
 <h2 id="rating-bank">Рейтинг Банков</h2> <h2 id="rating-micro">Рейтинг микроззаймов</h2>
 <?php get_template_part('inc/table'); ?>
